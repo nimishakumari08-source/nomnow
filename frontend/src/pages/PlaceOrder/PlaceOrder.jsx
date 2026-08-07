9960b4216f2ff910c260a65b6ef57b760d9cf08d
nimishakumari08-source <nimishakumari08@gmail.com>
2026-08-07 20:59:54 +0530

    Initial commit

---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './PlaceOrder.css'
+const PlaceOrder = () => {
+  return (
+    <div>PlaceOrder</div>
+  )
+}
+
+export default PlaceOrder
